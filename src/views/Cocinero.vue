44c2acf4fe24ae5911f97e7fc3cc4b812b158bf0
GloryRojas <grojasm94@gmail.com>
2019-07-01 22:08:19 -0500

    Adding boilerplate configuration

---
+++ src/views/Cocinero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Menu msg="Pedidos"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from '@/components/Menu.vue';
+
+export default {
+  name: 'Mesero',
+  components: {
+    Menu,
+  },
+};
+</script>
---
+++ src/views/Mesero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Menu msg="Mesero"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from '@/components/Menu.vue';
+
+export default {
+  name: 'Mesero',
+  components: {
+    Menu,
+  },
+};
+</script>
